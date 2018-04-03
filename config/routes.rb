7a5c4ed2d3dc5a402c22f10e9a5e825b32d3b980
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-03 10:42:59 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resourcese :comments, only: :create
+  end
   resources :users, only: [:show, :edit, :update]
 end
