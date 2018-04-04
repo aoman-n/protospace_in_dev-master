e8115cee9192cfd6e5c314d2556bdfee3cd9b0c9
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-04 10:54:06 +0900

    投稿機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
-  def create
 
+  def create
+    Comment.create(params_comment)
+    redirect_to controller: :prototypes, action: :index
+  end
+
+  private
+  def params_comment
+    params.permit(:content).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
   end
 end
