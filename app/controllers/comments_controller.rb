4d29324820162702b4568310282fe25f6a926179
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-05 12:02:19 +0900

    create_comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,23 @@
 class CommentsController < ApplicationController
 
+  before_action :set_prototype
+
+  def index
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
   def create
     Comment.create(params_comment)
-    redirect_to controller: :prototypes, action: :show, id: params[:prototype_id]
+    redirect_to action: :index
   end
 
   private
   def params_comment
-    params.permit(:content).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
