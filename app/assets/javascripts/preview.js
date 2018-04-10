100cb6b8240f58fe3ef0041fd994b23a46efcaef
rita1204 <pittixichan@gmail.com>
2018-04-10 12:14:26 +0900

    success_preview

---
+++ app/assets/javascripts/preview.js
@@ -0,0 +1,29 @@
+$(function(){
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('form').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".cover-image-upload");
+        t = this;
+
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $preview.empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "100%",
+                  overflow: "hidden",
+                  height: "auto",
+                  class: "cover-image-upload",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
