825cefefadf93860cea2be9c57d3d2ed3e6ac0ef
first commit <hiroaki@aobahiroakinoMacBook-Pro.local>
2018-04-19 12:14:58 +0900

    Fix

---
+++ app/assets/javascripts/flash.js
@@ -0,0 +1,12 @@
+// $(function () {
+//     setTimeout('rect()'); //アニメーションを実行
+// });
+
+// function rect() {
+//     $(".btn-group").animate({
+//         left: "350px" //要素を動かす位置
+//     }, 3000).animate({
+//         left: "-600px"　//要素を戻す位置
+//     }, 0)
+//     setTimeout("rect()", 3000);//アニメーションを繰り返す間隔
+// }
--- app/views/comments/like.json.jbuilder
+++ app/views/comments/like.json.jbuilder
@@ -1,2 +1,4 @@
 json.count @count
 json.like_count @like_count
+json.current_user user_signed_in?
+json.array @liked_user_array.include?(current_user.id)
